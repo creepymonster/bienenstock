9fe79de16a235aee76ba9d55839be3ff716e2dbb
Reimar Metzen <reimar.metzen@iti-software.de>
2020-08-10 08:41:37 +0200

    Split app.component, remove inline template/styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-    </div>   
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'bienenstock';
