f4eedf0df3483dd8a56d28061319d398a1a46353
Reimar Metzen <creepymonster@users.noreply.github.com>
2020-08-15 12:11:39 +0200

    Fix x axis time format

--- src/app/components/weight-view/weight-view.component.ts
+++ src/app/components/weight-view/weight-view.component.ts
@@ -237,7 +237,7 @@ export class WeightViewComponent implements OnChanges {
                     }
 
                     const dateValue = new Date(timestamp);
-                    return `${dateValue.toLocaleTimeString(this.locale, {hour: '2-digit', minute:'2-digit'})}`;
+                    return `${dateValue.toLocaleTimeString(this.locale, {hour: '2-digit'})}`;
                   }
                 },
                 axisBorder: {
